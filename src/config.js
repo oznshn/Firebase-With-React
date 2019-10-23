1bdb1e2f33e41dbb3ffdca2cedaad42f56555f95
oznshn <oznshn@gmail.com>
2019-10-23 22:27:14 +0200

    change config

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
   var config = {
-    apiKey: "AIzaSyCcjlYBncZlXe4fS3f1VHdKBxxJnjLhkAU",
-    authDomain: "assigmentbackend.firebaseapp.com",
-    databaseURL: "https://assigmentbackend.firebaseio.com",
-    projectId: "assigmentbackend",
-    storageBucket: "assigmentbackend.appspot.com",
-    messagingSenderId: "213990196921",
-    appId: "1:213990196921:web:9c93189c13a486049e7d68",
-    measurementId: "G-EVT60P9NYC"
+    apiKey: "###
+    authDomain: "###",
+    databaseURL: "#####",
+    projectId: "#####",
+    storageBucket: "#####",
+    messagingSenderId: "#####",
+    appId: "#####",
+    measurementId: "#####"
   };
   // Initialize Firebase
   export default config;
